fec0d60364196db1e39d71e2281c006f64efd9d2
DrSkyRitik-15 <ritiksinghkushwaha06@gmail.com>
2025-12-10 21:07:13 +0530

    feat: implement UserComponent with Login, Profile, and Setting components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
 import Header from "./Header";
+import Login,{Profile,Setting,UserKey} from "./UserComponent";  
+
+function App(){
+
+  const data = "Ritik Made this ";
+  return (
+    <div>
+      <h1>This is JSX DATA {data}</h1>
+      <h1> The sum of 2+2 is {2+2} </h1>
+    </div>
+  )
+}
+
+
+
+
+
+
+
+/*
 function App(){
 
   //alert("The sum of 2+2 is " + sum());
   return (
     <div>
-    
+    <Header/>
+    <Login/>
+    <Profile/>
+    <Setting/>
+    <h1>{UserKey}</h1>
     <h1>Apple Component</h1>
 
     <Fool/>
@@ -21,12 +45,10 @@ function Fool(){
 }
 
 
-/*function sum(){
+function sum(){
   return 2+2;
 }
+
   */
-
-
-
-export default App; 
 // i m making new file header.jsx and moving header component there can see below
+export default App; 
---
+++ src/UserComponent.jsx
@@ -0,0 +1,22 @@
+function Login(){
+
+
+    return (
+        <h1> Login Component </h1>
+    );
+}
+
+ export function Profile(){
+    return (
+        <h1> Profile Component ❤ </h1>
+    );
+}
+export function Setting(){
+    return (
+        <h1> Setting Component ❤ </h1>
+    );
+}
+
+export const UserKey = "some_user_key_value";
+
+export default Login ;
